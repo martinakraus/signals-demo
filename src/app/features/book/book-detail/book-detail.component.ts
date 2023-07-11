e192fd5033a2347f04ab945a21b682647870bf64
Martina Kraus <kraus.martina.m@gmail.com>
2023-07-11 16:07:28 +0200

    refactoring

--- src/app/features/book/book-detail/book-detail.component.ts
+++ src/app/features/book/book-detail/book-detail.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { BookApiService } from '../services/book-api.service';
-import { Book } from '../models/book';
-import { Observable, switchMap, tap } from 'rxjs';
+import { switchMap, tap } from 'rxjs';
 import { TranslateModule } from '@ngx-translate/core';
 import { CommonModule } from '@angular/common';
 import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
@@ -15,18 +14,15 @@ import { TitleService } from "../../../services/title.service";
     imports: [ TranslateModule, CommonModule, MatButtonModule, RouterLink ],
     styleUrls: [ './book-detail.component.scss' ],
 })
-export class BookDetailComponent implements OnInit {
-    book$!: Observable<Book>;
+export class BookDetailComponent {
+    bookApiService = inject(BookApiService);
+
+    book$ = this.route.params.pipe(
+        switchMap((params) => this.bookApiService.getByIsbn(params['isbn'])),
+        tap((book) => this.titleService.setAppTitle(book.title))
+    );
 
     constructor(private readonly route: ActivatedRoute,
-                private readonly bookApiService: BookApiService,
                 private readonly titleService: TitleService) {
-    }
-
-    ngOnInit(): void {
-        this.book$ = this.route.params.pipe(
-            switchMap((params) => this.bookApiService.getByIsbn(params['isbn'])),
-            tap((book) => this.titleService.setAppTitle(book.title))
-        );
     }
 }
--- src/app/features/book/book/book.component.ts
+++ src/app/features/book/book/book.component.ts
@@ -1,11 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Book } from '../models/book';
 import { BookApiService } from '../services/book-api.service';
-import { Observable, Subscription } from 'rxjs';
 import { Router } from '@angular/router';
 import { BookCardComponent } from '../book-card/book-card.component';
 import { CommonModule } from '@angular/common';
-import { Title } from "@angular/platform-browser";
 import { TitleService } from "../../../services/title.service";
 
 @Component({
@@ -15,17 +13,11 @@ import { TitleService } from "../../../services/title.service";
     imports: [ BookCardComponent, CommonModule ],
     styleUrls: [ './book.component.scss' ],
 })
-export class BookComponent implements OnInit {
-    books$!: Observable<Book[]>;
-    subscription!: Subscription;
+export class BookComponent {
+    books$ = inject(BookApiService).getAll();
 
-    constructor(private readonly bookApiService: BookApiService,
-                private readonly router: Router,
+    constructor(private readonly router: Router,
                 private readonly titleService: TitleService) {
-    }
-
-    ngOnInit(): void {
-        this.books$ = this.bookApiService.getAll();
         this.titleService.setAppTitle('Bookmonkey-App')
     }
 
